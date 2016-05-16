39369013ec08d3942d18bbbcc5bac6fcaa7f23d7
Michelle Casey <mcasey5216@gmail.com>
2016-05-16 14:40:23 -0400

    jade

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,10 @@ module.exports = function(grunt) {
       sass: {
         files: 'sass/*.scss',
         tasks: ['sass']
+      },
+      pug: {
+        files: 'jade/*.jade',
+        tasks: ['pug']
       }
     },
     concat: {
@@ -23,14 +27,36 @@ module.exports = function(grunt) {
           'css/style.css' : 'sass/style.scss'
         }
       }
-    }
+    },
+    pug: {
+          compile: {
+              options: {
+                  client: false,
+                  pretty: true,
+                  data: {
+                      debug: false
+                  }
+              },
+              files: [
+              {
+                  'dist/index.html': ['jade/index.jade']
+              },
+              {
+                  src: "jade/*.jade",
+                  dest: "dist",
+                  expand: true,
+                  ext: ".html"
+              } ]
+          }
+      }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-pug');
 
 
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['sass','pug']);
 
 };
--- dist/my-project-name.js
+++
@@ -1,7 +0,0 @@
-(function (Controller, undefined){
-  console.log('bludiblop');
-  var a = "Controller Name";
-}(Main.Contoller == Main.Controller || {} ));
-;(function (Main, undefined) {
-    console.log('lalala');
-}(window.Main == window.Main || {} ));
